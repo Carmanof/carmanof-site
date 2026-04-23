e377420db2bb0b0b07283968e2d28c0d91208796
Aleksei <l.mayson315@gmail.com>
2026-04-23 23:12:07 +0300

    Fix intro animation target and update cookie button text

--- src/components/CookieConsent/CookieConsent.tsx
+++ src/components/CookieConsent/CookieConsent.tsx
@@ -126,7 +126,7 @@ export default function CookieConsent() {
             cursor: "pointer",
           }}
         >
-          Только обязательные
+          Не принимаю
         </button>
       </div>
     </div>
