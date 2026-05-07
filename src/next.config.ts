90f7a228b6943855b3e61e3510671ba0f1accd8b
Aleksei <l.mayson315@gmail.com>
2026-05-07 20:50:25 +0300

    fix: disable next image optimization

--- src/next.config.ts
+++ src/next.config.ts
@@ -4,6 +4,8 @@ const MAIN_DOMAIN = "carmanof.ru";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
+
     remotePatterns: [
       { protocol: "https", hostname: "cdn.sanity.io" },
       { protocol: "https", hostname: "i.ytimg.com" },
