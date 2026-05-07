46fd7fb4e598d377c7ab0476d89e3ea4eafe1540
Aleksei <l.mayson315@gmail.com>
2026-05-07 21:42:05 +0300

    debug: disable deferred scripts

--- src/components/DeferredScripts.tsx
+++ src/components/DeferredScripts.tsx
@@ -1,21 +1,3 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const Analytics = dynamic(() => import("@/components/Analytics/Analytics"), {
-  ssr: false,
-});
-
-const CookieConsent = dynamic(
-  () => import("@/components/CookieConsent/CookieConsent"),
-  { ssr: false },
-);
-
 export default function DeferredScripts() {
-  return (
-    <>
-      <Analytics />
-      <CookieConsent />
-    </>
-  );
+  return null;
 }
