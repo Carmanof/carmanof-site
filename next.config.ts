b40ceea0941676d10e13dfb27cda86960a0de2d5
Aleksei <l.mayson315@gmail.com>
2026-05-08 22:30:08 +0300

    fix: disable trailing slash to fix Vercel static loop

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
+  trailingSlash: false,
+
   images: {
     unoptimized: true,
     remotePatterns: [
